0906926e28f7e7d1f060b0cf6d86850e966a12e8
ChrstnGrx <christinegroux1@gmail.com>
2021-12-22 14:49:05 -0500

    new icon and title

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          How about I don't.
         </p>
         <a
           className="App-link"
